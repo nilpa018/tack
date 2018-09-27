98ca516320a40acd2e5650e5280501003089d368
Loïc Marchand <nilpa018@yahoo.fr>
2018-09-27 12:24:58 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+import Clock from "./Timer";
+
+class App extends Component {
+  render() {
+    return (
+      <Clock/>
+    );
+  }
+}
+
+export default App;
---
+++ src/Timer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Clock extends Component {
+	constructor(){
+		super();
+		this.state = {
+			count: 0
+		}
+}
+componentDidMount() {
+    this.timer = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+    tick() {
+    this.setState({
+    	count: this.state.count + 1
+    });
+  }
+
+	render() {
+		return (
+			<div>
+			<h1>Vous avez ouvert cette page depuis {this.state.count} secondes</h1>
+			</div>
+		);
+	}
+}
+
+export default Clock;
